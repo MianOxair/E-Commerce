5e13c53eb9f7247258975a45904a3e68b00b57ab
MianOxair <103268670+MianOxair@users.noreply.github.com>
2022-12-03 04:57:16 -0800

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Hello from Mian & Bahman</div>;
+
+export default App;
---
+++ client/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: 'Any',
+    array: [],
+  },
+  {
+    _id: 1,
+    name: '$0 to $9',
+    array: [0, 9],
+  },
+  {
+    _id: 2,
+    name: '$10 to $99',
+    array: [10, 99],
+  },
+  {
+    _id: 3,
+    name: '$100 to $499',
+    array: [100, 499],
+  },
+  {
+    _id: 4,
+    name: '$500 to $999',
+    array: [500, 999],
+  },
+  {
+    _id: 5,
+    name: 'More than 999',
+    array: [1000, 99999],
+  },
+];

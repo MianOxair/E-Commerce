63b5df6e5f686a6eb6f489061112ea8b10a621e3
MianOxair <103268670+MianOxair@users.noreply.github.com>
2022-12-07 22:26:29 -0800

    Update UserDashboard.js

--- client/src/user/UserDashboard.js
+++ client/src/user/UserDashboard.js
@@ -78,7 +78,9 @@ const Dashboard = () => {
                       <div key={i}>
                         <h6>Product name: {p.name}</h6>
                         <h6>Product price: ${p.price}</h6>
-                        <h6>Purchased date: {moment(p.createdAt).fromNow()}</h6>
+                        <h6>Total products in the order: {p.count}</h6>
+                        <h6>Total: ${p.count*p.price}</h6>
+                        {/* <h6>Purchased date: {moment(p.createdAt).fromNow()}</h6> */}
                       </div>
                     );
                   })}
